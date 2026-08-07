686d77662f457b867ac3a17eecd99afbf1e814ba
Mohammad Riaz <mohammadriaz60000@gmail.com>
2026-08-07 10:33:12 +0600

    setting up project

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from "./app"
+
+
+
+
+const bootstrap = ()=>{
+    try {
+        app.listen(5000, () => {
+  console.log(`Server is running on http://localhost:5000`);
+});
+    } catch (error) {
+        console.error("failed error ", error)
+    }
+}
+
+
+bootstrap();
